c1c1a1924a8d05edfb20e64226101607a9c2e767
Jacob Hart <jacobhart84@hotmail.com>
2013-06-02 22:37:27 -0500

    removed table from location

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -4,6 +4,7 @@ class IndustriesController < ApplicationController
   def index
     @industries = Industry.all
 
+# Example for Dan...
     @industry = Industry.first
     @companies = Company.where(industry_id: @industry.id)
     @fundings = []
@@ -12,6 +13,7 @@ class IndustriesController < ApplicationController
         @fundings << (Funding.find_by_company_id(company.id))
       end
     end
+    # end example
 
     respond_to do |format|
       format.html # index.html.erb
