b01b956206fd1f160891f21ac72bf7402958be62
Jacob Hart <jacobhart84@hotmail.com>
2013-06-08 23:20:26 -0500

    extra unlesses

--- db/seeds/run.seeds.rb
+++ db/seeds/run.seeds.rb
@@ -8,7 +8,7 @@
 # Location.destroy_all
 
   run_number = 0
-  number_of_queries = 1000
+  number_of_queries = 100
 # bundle install other macs
 # ____________________________________________________________
 
@@ -40,151 +40,150 @@ companies = Company.offset(run_number * number_of_queries).limit(number_of_queri
 companies.each_index do |i|
 
   # error 1)
-  If JSON.parse(open(base_url+co+companies[i].perma+".js?"+api_key).read)
+
     company_data = JSON.parse(open(base_url+co+companies[i].perma+".js?"+api_key).read)
+    unless Company.find_by_perma(company_data["permalink"]) == nil
+      c = Company.find_by_perma(company_data["permalink"])
 
-    c = Company.find_by_perma(company_data["permalink"])
-
-    if company_data["category_code"] == nil
-      c.industry_id = Industry.first.id
-    elsif Industry.find_by_name(company_data["category_code"]) == nil
-      Industry.create(name: company_data["category_code"])
-      c.industry_id = Industry.find_by_name(company_data["category_code"]).id
-    else
-      c.industry_id = Industry.find_by_name(company_data["category_code"]).id
-    end
-
-    l = Location.new
-    l.address1 = company_data['offices'][0]['address1']
-    l.address2 = company_data['offices'][0]['address12']
-    l.zipcode = company_data['offices'][0]['zip_code']
-    l.city = company_data['offices'][0]['city']
-    l.statecode = company_data['offices'][0]['state_code']
-    l.countrycode = company_data['offices'][0]['country_code']
-    if company_data['offices'][0]['latitude'].present?
-      l.latitude = company_data['offices'][0]['latitude']
-      l.longitude = company_data['offices'][0]['longitude']
-    end
-    l.company_id = c.id
-    l.save
-
-
-    puts "There are #{Company.all.count} companies in the database"
-
-# -------------- Company Funding Rounds -------------- #
-
-
-    company_data["funding_rounds"].each do |round|
-      f = Funding.new
-      f.company_id = c.id
-
-
-      if round["round_code"] == "unattributed"
-        f.funding_code = "venture round"
+      if company_data["category_code"] == nil
+        c.industry_id = Industry.first.id
+      elsif Industry.find_by_name(company_data["category_code"]) == nil
+        Industry.create(name: company_data["category_code"])
+        c.industry_id = Industry.find_by_name(company_data["category_code"]).id
       else
-        f.funding_code = round["round_code"]
+        c.industry_id = Industry.find_by_name(company_data["category_code"]).id
       end
 
-      if round["raised_amount"] != nil
-        f.funding_amount = round["raised_amount"]
-      else
-        f.funding_amount = nil
+      l = Location.new
+      l.address1 = company_data['offices'][0]['address1']
+      l.address2 = company_data['offices'][0]['address12']
+      l.zipcode = company_data['offices'][0]['zip_code']
+      l.city = company_data['offices'][0]['city']
+      l.statecode = company_data['offices'][0]['state_code']
+      l.countrycode = company_data['offices'][0]['country_code']
+      if company_data['offices'][0]['latitude'].present?
+        l.latitude = company_data['offices'][0]['latitude']
+        l.longitude = company_data['offices'][0]['longitude']
       end
+      l.company_id = c.id
+      l.save
 
-      if round["raised_currency_code"] != nil
-        f.funding_currency = round["raised_currency_code"]
-      else
-        f.funding_currency = nil
-      end
 
-      if round["funded_year"] == nil
-          f.funding_date = nil
-        elsif round["funded_month"] == nil
-          f.funding_date = Date.new( round["funded_year"] )
-        elsif round["funded_day"] == nil
-          f.funding_date = Date.new( round["funded_year"], round["funded_month"])
+      puts "There are #{Company.all.count} companies in the database"
+
+  # -------------- Company Funding Rounds -------------- #
+
+
+      company_data["funding_rounds"].each do |round|
+        f = Funding.new
+        f.company_id = c.id
+
+
+        if round["round_code"] == "unattributed"
+          f.funding_code = "venture round"
         else
-          f.funding_date = Date.new(round["funded_year"], round["funded_month"], round["funded_day"])
-      end
+          f.funding_code = round["round_code"]
+        end
 
-      f.save
-      puts "There are #{Funding.all.count} funding rounds in the database"
+        if round["raised_amount"] != nil
+          f.funding_amount = round["raised_amount"]
+        else
+          f.funding_amount = nil
+        end
 
-# ------------------Investments
+        if round["raised_currency_code"] != nil
+          f.funding_currency = round["raised_currency_code"]
+        else
+          f.funding_currency = nil
+        end
 
-# WAT KUNNEN WE HIER MEE?!
-      if round["investments"].empty?
-        i = Investment.new
-        i.funding_id = f.id
-        i.save
-      end
+        if round["funded_year"] == nil
+            f.funding_date = nil
+          elsif round["funded_month"] == nil
+            f.funding_date = Date.new( round["funded_year"] )
+          elsif round["funded_day"] == nil
+            f.funding_date = Date.new( round["funded_year"], round["funded_month"])
+          else
+            f.funding_date = Date.new(round["funded_year"], round["funded_month"], round["funded_day"])
+        end
 
-      round["investments"].each do |investment|
+        f.save
+        puts "There are #{Funding.all.count} funding rounds in the database"
 
-      if investment["company"] != nil
-          company = Company.find_by_perma(investment["company"]["permalink"])
-          company.investments.create(funding_id: f.id)
+  # ------------------Investments
 
-      elsif investment["financial_org"] != nil
-        financial = Financial.find_by_perma(investment["financial_org"]["permalink"])
-        financial.investments.create(funding_id: f.id)
-
-        i = Investment.new
-        i.funding_id = f.id
-# Niet nodig fin perma?!
-        i.financial_perma = investment["financial_org"]["permalink"]
-        i.investable_id = Financial.find_by_perma(i.financial_perma).id
-        i.investable_type="financial"
-        i.save
-
-        elsif investment["person"] != nil
-          individual = Individual.find_by_perma(investment["person"]["permalink"])
-          individual.investments.create(funding_id: f.id)
-
+  # WAT KUNNEN WE HIER MEE?!
+        if round["investments"].empty?
           i = Investment.new
           i.funding_id = f.id
-# indiv perma?!
-          i.individual_perma = investment["person"]["permalink"]
-          i.investable_id = Individual.find_by_perma(i.individual_perma).id
-          i.investable_type="individual"
           i.save
+        end
 
-        else
+        round["investments"].each do |investment|
 
-          i = Investment.new
-          i.funding_id = f.id
-          i.investable_id = nil
-          i.save
+        if investment["company"] != nil
+          unless Company.find_by_perma(investment["company"]["permalink"]) == nil
+            company = Company.find_by_perma(investment["company"]["permalink"])
+            company.investments.create(funding_id: f.id)
+          end
+        elsif investment["financial_org"] != nil
+          unless Financial.find_by_perma(investment["financial_org"]["permalink"]) == nil
+            financial = Financial.find_by_perma(investment["financial_org"]["permalink"])
+            financial.investments.create(funding_id: f.id)
+
+            i = Investment.new
+            i.funding_id = f.id
+            i.investable_id = Financial.find_by_perma(investment["financial_org"]["permalink"]).id
+            i.investable_type="financial"
+            i.save
+          end
+          elsif investment["person"] != nil
+            unless Individual.find_by_perma(investment["person"]["permalink"]) == nil
+            individual = Individual.find_by_perma(investment["person"]["permalink"])
+            individual.investments.create(funding_id: f.id)
+
+            i = Investment.new
+            i.funding_id = f.id
+            i.investable_id = Individual.find_by_perma(investment["person"]["permalink"]).id
+            i.investable_type="individual"
+            i.save
+            end
+          else
+
+            i = Investment.new
+            i.funding_id = f.id
+            i.investable_id = nil
+            i.save
+          end
+
         end
 
       end
 
+      if company_data["ipo"] != nil
+
+        f = Funding.new
+        f.company_id = c.id
+        f.funding_code = 'ipo'
+        f.funding_amount = company_data["ipo"]["valuation_amount"]
+        f.funding_currency = company_data["ipo"]["valuation_currency_code"]
+
+
+      if company_data["ipo"]["pub_year"] == nil
+        f.funding_date = nil
+      elsif company_data["ipo"]["pub_month"] == nil
+        f.funding_date = Date.new(company_data["ipo"]["pub_year"])
+      elsif company_data["ipo"]["pub_day"] == nil
+        f.funding_date = Date.new(company_data["ipo"]["pub_year"], company_data["ipo"]["pub_month"])
+      else
+        f.funding_date = Date.new(company_data["ipo"]["pub_year"], company_data["ipo"]["pub_month"], company_data["ipo"]["pub_day"] )
+      end
+
+      f.save
+
+      end
+
     end
-
-    if company_data["ipo"] != nil
-
-      f = Funding.new
-      f.company_id = c.id
-      f.funding_code = 'ipo'
-      f.funding_amount = company_data["ipo"]["valuation_amount"]
-      f.funding_currency = company_data["ipo"]["valuation_currency_code"]
-
-
-    if company_data["ipo"]["pub_year"] == nil
-      f.funding_date = nil
-    elsif company_data["ipo"]["pub_month"] == nil
-      f.funding_date = Date.new(company_data["ipo"]["pub_year"])
-    elsif company_data["ipo"]["pub_day"] == nil
-      f.funding_date = Date.new(company_data["ipo"]["pub_year"], company_data["ipo"]["pub_month"])
-    else
-      f.funding_date = Date.new(company_data["ipo"]["pub_year"], company_data["ipo"]["pub_month"], company_data["ipo"]["pub_day"] )
-    end
-
-    f.save
-
-    end
-
-  end
 end
 end_time = Time.now
 puts "It took #{end_time - start_time} seconds to run"
