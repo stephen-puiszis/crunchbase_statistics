5ea0764a9eca32ea2cf09d6ffa85a3cdf133e3ac
stephen-puiszis <spuiszis@gmail.com>
2013-06-12 00:27:55 -0500

    Made more changes

--- db/seeds/industry.seeds.rb
+++ db/seeds/industry.seeds.rb
@@ -1,23 +1,24 @@
 Industry.create([
-  { :name => "other" },
-  { :name => "advertising" },
-  { :name => "biotech" },
-  { :name => "cleantech" },
-  { :name => "hardware" },
-  { :name => "web" },
-  { :name => "ecommerce" },
-  { :name => "education" },
-  { :name => "enterprise" },
-  { :name => "games_video" },
-  { :name => "legal" },
-  { :name => "mobile" },
-  { :name => "network_hosting" },
-  { :name => "consulting" },
-  { :name => "public_relations" },
-  { :name => "search" },
-  { :name => "security" },
-  { :name => "semiconductor" },
-  { :name => "software" }
+  { :name => "other", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "web", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "enterprise", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "software", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "network_hosting", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "games_video", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "search", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "mobile", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "advertising", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "ecommerce", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "hardware", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "public_relations", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "security", :created_at => "2013-06-06 04:57:27", :updated_at => "2013-06-06 04:57:27" },
+  { :name => "consulting", :created_at => "2013-06-06 04:57:28", :updated_at => "2013-06-06 04:57:28" },
+  { :name => "education", :created_at => "2013-06-06 04:57:29", :updated_at => "2013-06-06 04:57:29" },
+  { :name => "biotech", :created_at => "2013-06-06 04:57:30", :updated_at => "2013-06-06 04:57:30" },
+  { :name => "semiconductor", :created_at => "2013-06-06 04:57:31", :updated_at => "2013-06-06 04:57:31" },
+  { :name => "cleantech", :created_at => "2013-06-06 04:57:31", :updated_at => "2013-06-06 04:57:31" },
+  { :name => "legal", :created_at => "2013-06-06 04:57:32", :updated_at => "2013-06-06 04:57:32" }
 ], :without_protection => true )
 
 puts "There are #{Industry.count} database records"
+puts Industry.last.id
